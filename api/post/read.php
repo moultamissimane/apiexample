f566601bc44054ea760b20fbbe90f8f881ac02be
moultamissimane <93978575+moultamissimane@users.noreply.github.com>
2022-03-09 22:06:00 +0100

    add Database.php and read.php

--- api/post/read.php
+++ api/post/read.php
@@ -13,3 +13,33 @@ $db = $database->connect();
 $post = new Post($db);
 // blog post query 
 $result = $post->read();
+// get row count
+$num = $result->rowCount();
+// vheck if any posts
+if ($num > 0) {
+    // post array
+    $posts_arr = array();
+    $posts_arr['data'] = array();
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+        $post_item = array(
+            'id' =>$id,
+            'title'=>$title,
+            'body'=>html_entity_decode($body),
+            'author'=>$author,
+            'category_id'=>$category_id,
+            'category_name'=>$category_name
+        );
+        // push to data
+        array_push($posts_arr['data'], $post_item);
+        // turn to JSON & output
+        echo json_encode($posts_arr);
+    
+    }
+} else {
+    // no posts
+    echo json_encode(
+        array('message' => 'No Posts Found')
+    );
+}
+
